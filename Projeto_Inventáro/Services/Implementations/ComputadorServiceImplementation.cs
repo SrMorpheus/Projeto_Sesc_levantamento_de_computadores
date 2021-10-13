6fef82af5b23f32fb28884886553dbb14c8c1017
Bruno Gonçalves Barros <bbarros@sescap.com.br>
2021-10-13 11:44:37 -0300

    versão com CRUD dos computadores

--- Projeto_Inventáro/Services/Implementations/ComputadorServiceImplementation.cs
+++ Projeto_Inventáro/Services/Implementations/ComputadorServiceImplementation.cs
@@ -26,6 +26,22 @@ namespace Projeto_Inventáro.Services.Implementations
         public Computador Create(Computador computador)
         {
 
+            try
+            {
+
+                _context.Add(computador);
+
+                _context.SaveChanges();
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+
+
             return computador;
 
 
@@ -33,7 +49,27 @@ namespace Projeto_Inventáro.Services.Implementations
 
         public void Delete(int id)
         {
-            
+
+
+            var result = _context.Computadors.SingleOrDefault(M => M.IdComputador.Equals(id));
+
+            if(result != null)
+            {
+
+                try
+                {
+
+                    _context.Computadors.Remove(result);
+
+                    _context.SaveChanges();
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
 
         }
 
@@ -51,39 +87,54 @@ namespace Projeto_Inventáro.Services.Implementations
         public Computador FindById(int id)
         {
 
-            return new Computador
-            {
-                IdComputador = 1,
 
-
-                NomeComptador = "BRUNO",
-
-                EnderecoIp = "190.120.2.115",
-
-                ModeloId = 1,
-
-                EquipamentoId = 1,
-
-                PatrimonioMonitor = 1266,
-
-                PatrimonioGabinete = 1958,
-
-                Ano = 2019,
-
-                Internet = true,
-
-                SetorId = 1
-
-
-            };
+            return _context.Computadors.SingleOrDefault(M => M.IdComputador.Equals(id));
 
         }
 
         public Computador Update(Computador computador)
         {
+            if (!Exists(computador.IdComputador)) return new Computador();
+
+
+            var result = _context.Computadors.SingleOrDefault(M => M.IdComputador.Equals(computador.IdComputador));
+
+
+            if (result != null)
+            {
+
+                try
+                {
+
+                    _context.Entry(result).CurrentValues.SetValues(computador);
+
+                    _context.SaveChanges();
+
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+
+                }
+
+
+
+            }
+
+
 
 
             return computador;
+
+
         }
+
+        private bool Exists(int id)
+        {
+
+
+            return _context.Computadors.Any(M => M.IdComputador.Equals(id));
+    }
     }
 }
